dc3d063314db1477d37a4f467e58cb0ba6239433
Kevin Kuchta <kevin@kevinkuchta.com>
2017-05-14 20:48:40 -0700

    Don't reply to replies to me.

--- screambot.rb
+++ screambot.rb
@@ -21,7 +21,13 @@ home_timeline do |tweet|
   original_id = tweet.id
   name = tweet.user.screen_name
   puts "Got tweet: #{tweet.text}"
-  video = Scream.new(tweet.text).make_video
-  reply("@#{name}", tweet, media: File.new(video))
-  puts "Posted response"
+
+  # Skip replies to my posts to avoid infinite bot cycles
+  unless tweet.in_reply_to_screen_name == client.user.screen_name
+    video = Scream.new(tweet.text).make_video
+    reply("@#{name}", tweet, media: File.new(video))
+    puts "Posted response"
+  else
+    puts "Was a reply to me– skip"
+  end
 end
